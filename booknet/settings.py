9a4e29aec580005163e85675e963cdc16f12629a
sneyder1711 <64097040+sneyder1711@users.noreply.github.com>
2025-06-23 19:25:11 -0500

    add apps to settings

--- booknet/settings.py
+++ booknet/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuarios',
+    'libros',
+    'compras',
+    'comentarios',
+    'recomendaciones',
 ]
 
 MIDDLEWARE = [
