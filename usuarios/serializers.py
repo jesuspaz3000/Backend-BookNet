6fc1c5b8223cabc7b79c345b0ec45fea9a8ae4bd
sneyder1711 <64097040+sneyder1711@users.noreply.github.com>
2025-06-29 03:09:37 -0500

    Add models serializers and views to Usuarios

---
+++ usuarios/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Usuario
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        read_only_fields = ['id']
+    
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Usuario
+from .serializers import UsuarioSerializer
+
 
 # Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            queryset = queryset.filter(username=username)
+        return queryset
